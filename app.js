0c1c4827975e3863979bc91b63a091b508efd03a
Omar Meharab <omarmeharab81@gmail.com>
2021-02-09 11:57:16 +0600

    was able to show photos now have to style them

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
-    .then(res => res.json())
-    .then(data => console.log(data));
+const searchFoods = () => {
+    const searchInput = document.getElementById('search-input').value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMeals(data.meals));
+}
+
+const displayMeals = meals => {
+    const mealNamesDiv = document.getElementById('meal-containers');
+
+    meals.forEach(meals => {
+        console.log(meals.strMealThumb);
+        const individualMealDivs = document.createElement('div');
+        individualMealDivs.className = 'individual-divs';
+        individualMealDivs.innerHTML = `
+        <div>
+            <img src="${meals.strMealThumb}">
+            <p id="meal-names">${meals.strMeal}</p>
+        </div>`
+        mealNamesDiv.appendChild(individualMealDivs);
+        console.log(mealNamesDiv);
+    });
+}
+
