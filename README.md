689759237bc5aba0d404c6421adcdbe5ef8b4f07
Omar Meharab <omarmeharab81@gmail.com>
2021-02-10 23:08:31 +0600

    finally done alhamdulillah.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ###
 meal name links: https://www.themealdb.com/api/json/v1/1/search.php?s=searchName
+ingredients link: https://www.themealdb.com/api/json/v1/1/lookup.php?i=idMeal
 meals
 strMeal
 strMealThumb
+strIngredient
--- app.js
+++ app.js
@@ -3,22 +3,57 @@ const searchFoods = () => {
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`
     fetch(url)
         .then(res => res.json())
-        .then(data => displayMeals(data.meals));
+        .then(data => displayMeals(data.meals))
+        .catch(error => displayError('Please write your meal name in the search box.'));
 }
 
 const displayMeals = meals => {
     const mealNamesDiv = document.getElementById('meal-containers');
     mealNamesDiv.innerHTML = '';
     meals.forEach(meals => {
-        console.log(meals.strMealThumb);
         const individualMealDivs = document.createElement('div');
         individualMealDivs.className = 'individual-divs';
         individualMealDivs.innerHTML = `
-        <div>
+        <div onclick="getMealId('${meals.idMeal}')">
             <img src="${meals.strMealThumb}">
             <p id="meal-names">${meals.strMeal}</p>
         </div>`
         mealNamesDiv.appendChild(individualMealDivs);
-        console.log(mealNamesDiv);
     });
+}
+
+const getMealId = idMeal => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayInfo(data.meals[0]))
+        .catch(error => displayError('Something seems to be a problem. Please come back later.'));
+}
+
+const displayInfo = ingredients => {
+    const infoContainer = document.getElementById('info-container');
+    const infoContainerDiv = document.createElement('div');
+    infoContainerDiv.className = 'info-box';
+    infoContainerDiv.innerHTML = `
+    <img src="${ingredients.strMealThumb}">
+    <h3>${ingredients.strMeal}</h3>
+    <h5>Ingredients</h5>
+    <ul>
+        <li>${ingredients.strIngredient1}</li>
+        <li>${ingredients.strIngredient2}</li>
+        <li>${ingredients.strIngredient3}</li>
+        <li>${ingredients.strIngredient4}</li>
+        <li>${ingredients.strIngredient5}</li>
+        <li>${ingredients.strIngredient6}</li>
+        <li>${ingredients.strIngredient7}</li>
+        <li>${ingredients.strIngredient8}</li>
+        <li>${ingredients.strIngredient9}</li>
+        <li>${ingredients.strIngredient10}</li>
+    </ul>`
+    infoContainer.appendChild(infoContainerDiv);
+}
+
+const displayError = error => {
+    const errorElement = document.getElementById('error-message');
+    errorElement.innerText = error;
 }
